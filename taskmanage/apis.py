971c98781b3cc7d039efc03e13038803adf2f088
Takuho3650 <s1f102101536@iniad.org>
2022-06-13 11:25:39 +0900

    バグ修正

--- taskmanage/apis.py
+++ taskmanage/apis.py
@@ -3,19 +3,27 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import IndexTasksSerializer, ChecklistsSerializer, DetailTaskSerializer
 
 class TasksIndex(ModelViewSet):
+    serializer_class = IndexTasksSerializer
     queryset = Tasks.objects.order_by("deadline")
 
     def get_serializer_class(self, pk=None, *args, **kwargs):
         if self.action!='list':
             return DetailTaskSerializer
         else:
-            return IndexTasksSerializer
+            return self.serializer_class
             
     def create(self, request, *args, **kwargs):
         response = super(TasksIndex, self).create(request, *args, **kwargs)
-        response.data = {"uid": response.data["uuid"]}
+        response.data = response.data["uuid"]
         return response
 
 class ChecklistsIndex(ModelViewSet):
     serializer_class = ChecklistsSerializer
     queryset = Checklists.objects.all()
+
+    def get_queryset(self):
+        parent = self.request.query_params.get("parent_task")
+        if parent:
+            return Checklists.objects.filter(parent_task=parent)
+        else:
+            return self.queryset
