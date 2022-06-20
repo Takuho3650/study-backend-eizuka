ad3b11928b7c2df235e68c7a7cf54a4885686f79
Takuho3650 <s1f102101536@iniad.org>
2022-06-20 10:54:02 +0900

    04. Redisの有効化まで完了
    メモ：手順通りに進んではいるものの、何をやっているのかいまいち理解出来ていない。

---
+++ chatapp/app01/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView, RedirectView
+
+# Create your views here.
+
+class home(TemplateView):
+    template_name = "app01/chat.html"
+
+class redirecthome(RedirectView):
+    url = "http://127.0.0.1:8000/home/"
+rd_home = redirecthome.as_view()
