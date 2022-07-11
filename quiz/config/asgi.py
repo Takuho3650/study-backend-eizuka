1e04edb0229845fcfefc376f8958f40f3a2f50f6
Takuho3650 <s1f102101536@iniad.org>
2022-07-11 21:02:00 +0900

    途中まで

---
+++ quiz/config/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for quiz project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+from channels.routing import URLRouter
+from channels.auth import AuthMiddlewareStack
+import quiz.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+#application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter( {
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack( URLRouter( quiz.routing.websocket_urlpatterns ) ),
+} )
